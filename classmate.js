08a64ab461fc8279873c4d8a05756e6957d9a45f
Hriday Kundu <hridayk831@gmail.com>
2025-12-09 20:42:24 +0530

    Update classmate.js
    add dynamic card creation, birthday wish feature

--- classmate.js
+++ classmate.js
@@ -1,3 +1,219 @@
+const friends = [
+    {
+        name : "Hriday Kundu",
+        dept : "CSE",
+        sec : "A",
+        roll : "49",
+        image : "hriday.jpeg",
+        facebook : "https://www.facebook.com/hridoy.kundu.127",
+        instagram : "https://www.instagram.com/hriday_kundu5?igsh=OTRpMWRxZXFsb2Q0",
+        linkedin : "https://www.linkedin.com/in/hriday-kundu-5646b2379/",
+        github : "https://github.com/Hriday831"
+    },
+    {
+        name : "Kunal Mandal",
+        dept : "CSE",
+        sec : "A",
+        roll : "42",
+        image : "kunal42.jpeg",
+        facebook : "#",
+        instagram : "https://www.instagram.com/kun_al_31?igsh=MWUyeThtbGxzNWpxeA==",
+        linkedin : "#",
+        github : "#"
+    },
+    {
+        name : "Rahul Rakhit",
+        dept : "CSE",
+        sec : "A",
+        roll : "43",
+        image : "rahul43.jpeg",
+        facebook : "#",
+        instagram : "https://www.instagram.com/rr_elite_og?igsh=MTFxc2lpejN6dnc4MA==",
+        linkedin : "https://www.linkedin.com/in/rahul-rakshit-a1159b374?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        github : "#"
+    },
+    {
+        name : "Aditi Das",
+        dept : "CSE",
+        sec : "A",
+        roll : "12",
+        image : "aditi12.jpeg",
+        facebook : "https://www.facebook.com/share/1CqMeN491s/",
+        instagram : "https://www.instagram.com/awwditih?igsh=MTVpc3UzYWlqaGxudA==",
+        linkedin : "https://www.linkedin.com/in/aditi-das-6743b9379?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        github : "https://github.com/aditidas15"
+    },
+    {
+        name : "Mainak Kundu",
+        dept : "CSE",
+        sec : "A",
+        roll : "47",
+        image : "mainak47.jpeg",
+        facebook : "#",
+        instagram : "https://www.instagram.com/1mperfection._?igsh=aGpxMzZkNW9oOXZ4",
+        linkedin : "https://www.linkedin.com/in/mainakkundu2006/",
+        github : "https://www.github.com/Mainak-2006"
+    },
+    {
+        name : "Rinku Singha",
+        dept : "CSE",
+        sec : "A",
+        roll : "51",
+        image : "Rinku Singha-A-51.jpeg",
+        facebook : "#",
+        instagram : "https://www.instagram.com/dam_itx.rinku?igsh=aWlxbW1uajNla2U5&utm_source=qr",
+        linkedin : "#",
+        github : "#"
+    },
+    {
+        name : "Madhushree Mishra",
+        dept : "CSE",
+        sec : "A",
+        roll : "41",
+        image : "Madhushree Mishra.jpg",
+        facebook : "#",
+        instagram : "https://www.instagram.com/kaizen._10?igsh=MWh2dDZscThhYWp5MQ==",
+        linkedin : "#",
+        github : "#"
+    },
+    {
+        name : "demo name",
+        dept : "CSE",
+        sec : "B",
+        roll : "00",
+        image : "demo.jpg",
+        facebook : "#",
+        instagram : "#",
+        linkedin : "#",
+        github : "#"
+    },
+    {
+        name : "demo name",
+        dept : "CSE",
+        sec : "B",
+        roll : "00",
+        image : "demo.jpg",
+        facebook : "#",
+        instagram : "#",
+        linkedin : "#",
+        github : "#"
+    },
+    {
+        name : "demo name",
+        dept : "CSE",
+        sec : "B",
+        roll : "00",
+        image : "demo.jpg",
+        facebook : "#",
+        instagram : "#",
+        linkedin : "#",
+        github : "#"
+    },
+    {
+        name : "demo name",
+        dept : "CSE",
+        sec : "B",
+        roll : "00",
+        image : "demo.jpg",
+        facebook : "#",
+        instagram : "#",
+        linkedin : "#",
+        github : "#"
+    }
+];
+
+const classmates = [{name:"RINKU SINGH",dob:"17/03"},{name:"JAANVI AGARWAL",dob:"05/06"},{name:"SAMIM ALI KHAN",dob:"21/01"},{name:"MD KAIF ANSARI",dob:"25/09"},{name:"SPORSHO MONDAL",dob:"04/09"},{name:"SAMIM ANSARI",dob:"25/04"},{name:"AMAN KUMAR",dob:"01/07"},{name:"ARMAN PATRA",dob:"24/06"},{name:"MANOJ KUMAR YADAV",dob:"22/07"},{name:"SWANARGHA ROY",dob:"19/05"},{name:"PALAK KUMARI GUPTA",dob:"02/01"},{name:"PRINCE KUMAR PRASAD",dob:"20/10"},{name:"BIKRAM SINGH",dob:"14/07"},{name:"TRIDIP MONDAL",dob:"02/12"},{name:"DEBJANI MODAK",dob:"26/09"},{name:"PRATYUSHA BHOWMICK",dob:"22/06"},{name:"HIMANSHU SHAKYA",dob:"08/06"},{name:"ABHIJIT KUMAR GUPTA",dob:"14/05"},{name:"RAVISHANKAR JHA",dob:"12/05"},{name:"DEV BHATTACHARJEE",dob:"12/05"},{name:"SANAT KUMAR ROY",dob:"23/01"},{name:"SATYAM GUPTA",dob:"01/09"},{name:"RUDRANIL CHOUDHURY",dob:"08/08"},{name:"SUPARNA BHATTACHARYA",dob:"08/10"},{name:"ARPAN CHAKRABORTY",dob:"02/12"},{name:"SWARNADIP MAITRA",dob:"23/02"},{name:"SAMIR MONDAL",dob:"22/07"},{name:"BISWAJIT DAS",dob:"16/11"},{name:"SOUMYAJIT GHOSH",dob:"08/09"},{name:"AKASH HANSDA",dob:"06/03"},{name:"AJAY PRATAP SINGH",dob:"01/01"},{name:"PROJUKTA CHANDA",dob:"27/12"},{name:"ADITYA MANDAL",dob:"30/10"},{name:"MAINAK KUNDU",dob:"03/02"},{name:"AMARJIT SAHA",dob:"29/08"},{name:"MEDHA SAHA",dob:"12/12"},{name:"PRAYASH THAPA",dob:"05/09"},{name:"RAHUL RAKSHIT",dob:"24/02"},{name:"ANIRBAN DUTTA",dob:"20/09"},{name:"ANKIT SARKAR",dob:"12/07"},{name:"ASMITA BASAK",dob:"21/10"},{name:"REEHANA BISWAKARMA",dob:"02/12"},{name:"ANIKET DAS",dob:"10/02"},{name:"RIMA ROY",dob:"04/09"},{name:"MINU SAHA",dob:"07/03"},{name:"ABHINAV SHAH",dob:"27/04"},{name:"SUPRIYA ROY",dob:"06/11"},{name:"MADHUSHREE MISHRA",dob:"10/09"},{name:"ANURAG MODAK",dob:"15/01"},{name:"SHREYASEE GHOSH",dob:"10/02"},{name:"SAIKAT SINGHA",dob:"14/04"},{name:"ANI BHATTACHARJEE",dob:"07/01"},{name:"DEVBART SHARMA",dob:"21/06"},{name:"DHRUBA SHIL",dob:"23/11"},{name:"ZINNIA ISLAM",dob:"20/03"},{name:"SUFIA SALAM",dob:"14/05"},{name:"KRISHNA AGARWAL",dob:"25/08"},{name:"CHAYAN SUTRADHAR",dob:"08/01"},{name:"SOUMEN SAMANTA",dob:"01/12"},{name:"SAIKAT BISWAS",dob:"28/02"},{name:"JYOTI SHUVARA BANIK",dob:"05/09"},{name:"SOUMI DATTA",dob:"31/03"},{name:"ANURAG MAJUMDER",dob:"22/12"},{name:"SUSMITA SHARMA",dob:"14/11"},{name:"SOPRANO CHATTERJEE",dob:"10/03"},{name:"JEET GUHA",dob:"29/07"},{name:"SUBHOM GHOSH",dob:"10/04"},{name:"DANISH HUSSAIN",dob:"25/12"},{name:"DEBDOOT MAJUMDER",dob:"09/12"},{name:"ADRIJA SAHA PRAMANIK",dob:"10/04"},{name:"KRITAGYA PRADHAN",dob:"08/02"},{name:"SWASTIK BOSE",dob:"11/07"},{name:"PRITAM KUMAR SINGH",dob:"07/12"},{name:"MOHANA SARKAR",dob:"05/02"},{name:"DEBOSMITA GHOSH",dob:"15/04"},{name:"MD NAFEES KHAN",dob:"14/02"},{name:"ANIRBAN DEY SARKAR",dob:"22/09"},{name:"SANIYA",dob:"09/11"},{name:"BRATIN BARDHAN",dob:"17/11"},{name:"SNEHANGSHU ROY",dob:"05/09"},{name:"PRASENJIT DUTTA",dob:"31/05"},{name:"SHAMBHAVI SINGH",dob:"30/09"},{name:"KUNAL MANDAL",dob:"26/08"},{name:"ARITRI DAS",dob:"13/03"},{name:"SARBANI DAS",dob:"25/02"},{name:"DITI BHADRA",dob:"02/01"},{name:"HRIDAY KUNDU",dob:"07/01"},{name:"SANTOSH ROY",dob:"01/03"},{name:"ADITI DAS",dob:"15/08"},{name:"MITRAZA DAS",dob:"04/07"},{name:"YUVRAJ MONDAL",dob:"09/01"},{name:"PARTHIB DAS",dob:"25/01"},{name:"SWAGATA JOARDER",dob:"31/12"},{name:"PRIYA SUTRADHAR",dob:"03/08"},{name:"SNEHA SINGHA",dob:"05/03"},{name:"PINTU MAHANTI",dob:"07/07"},{name:"SOHAM HALDER",dob:"04/08"},{name:"BANTI AGARWAL",dob:"29/10"},{name:"MIT SHARMA",dob:"25/07"},{name:"SHREYASHI GUPTA",dob:"10/10"},{name:"SUBHAJIT GOPE",dob:"29/11"},{name:"TANMOY PAUL",dob:"25/06"},{name:"SUBHAM JHA",dob:"06/10"},{name:"PRAJEKTA SARKAR",dob:"12/01"},{name:"ANIRBAN BOSE",dob:"01/01"},{name:"PIYASI DAS",dob:"14/09"},{name:"NAVYA SINGH",dob:"18/02"},{name:"TANNI SHINGHA",dob:"09/02"},{name:"DEBRAJ ROY",dob:"09/08"},{name:"SUMIT PAUL",dob:"05/01"},{name:"MD SHAREEF KHAN",dob:"05/09"},{name:"BISHWAJEET SAH",dob:"22/10"},{name:"SAGNIK GUHA NEOGI",dob:"28/06"},{name:"SRIPARNA BASAK",dob:"06/05"},{name:"CHINTU BARMAN",dob:"20/09"},{name:"SARBARTHA PRAMANIK",dob:"01/09"},{name:"SOUMYADEEP DAS",dob:"06/09"},{name:"KUSHAL BASNET",dob:"03/10"},{name:"SK IRFAN",dob:"23/06"},{name:"TANISHQUE DAS",dob:"23/05"},{name:"SAWAN SINGH",dob:"21/05"},{name:"SUBHABRATA GHOSH",dob:"29/08"},{name:"PARTHA BAISHNAB",dob:"02/02"},{name:"SRIJAN SINHA",dob:"10/02"},{name:"PRIYA KAR",dob:"24/10"},{name:"SUBRATA MURMU",dob:"10/03"},{name:"DEEP DE",dob:"17/05"},{name:"ANIKET KUMAR GUPTA",dob:"02/06"},{name:"SNEHA DUGAR",dob:"27/06"},{name:"HRIDAY KUMAR SINGHA",dob:"01/09"},{name:"ABHISHEK SANYASI",dob:"10/09"},{name:"RIYA KHATUN",dob:"22/11"},{name:"MEGHDEEP KUNDU",dob:"09/10"},{name:"SOMEN BISWAS",dob:"11/06"},{name:"SANCHAYITA DAS",dob:"06/06"},{name:"NILADRI GHOSH",dob:"11/04"},{name:"SURAJ SRIWASTAVA",dob:"17/11"},{name:"AKASH PRASAD",dob:"25/05"},{name:"DEBASMITA MODAK",dob:"28/01"},{name:"SOUMAJIT DUTTA",dob:"29/03"},{name:"SNEHANGSHU DEY",dob:"15/07"},{name:"ANANYA ROY",dob:"13/03"},{name:"ANKITA DEB",dob:"07/03"},{name:"NANDITA SAHA",dob:"01/08"},{name:"DEEPJOY DEB",dob:"12/11"},{name:"PRIYADARSHANI BASU",dob:"05/06"},{name:"ARGHYADIP RAY",dob:"11/09"},{name:"RUBUL HUSSAIN SARKAR",dob:"12/01"},{name:"ARITRI ADHIKARI",dob:"23/11"},{name:"JENIVA SAHA",dob:"13/08"},{name:"EKLAVYA MISHRA",dob:"22/05"},{name:"JOYDEEP GHOSH",dob:"14/06"},{name:"SPORSHO MONDAL",dob:"04/09"},{name:"PRAJEKTA SARKAR",dob:"12/01"}];
+
+
+//store of wishes
+const wishes = [
+        "Wishing you a day filled with joy, laughter, and everything that makes you happiest! May this new year bring amazing adventures and success. 🎂🎈",
+        "May your day be as awesome as you are! Wishing you the very best on your birthday and a year ahead full of happiness and success. 🎉",
+        "Happy Birthday! Here's to another year of growth, opportunity, and making incredible memories. Enjoy your special day! 🌟",
+        "Sending you the biggest birthday wishes! Hope you get spoiled rotten and have the most fantastic day celebrating all that you are. 🥳",
+        "A huge Happy Birthday! May you achieve all your dreams this coming year. Cheers to you! 🥂"
+    ];
+
+// Select a random wish from the array and store it in randomWish
+const randomIndex = Math.floor(Math.random() * wishes.length);
+const randomWish = wishes[randomIndex];
+
+
+
+// function to create card
+function createCard(person) { 
+    return `<article class="team__member">
+                <div class="team__member__image">
+                    <img src="./Assets/${person.image}" alt="${person.name}">
+                </div>
+                <div class="team__member__info">
+                    <h4>${person.name}</h4>
+                    <p>Dept- ${person.dept}, Roll- ${person.roll}</p>
+                </div>
+                <div class="team__member__socials">
+                    <a href="${person.facebook}" target="_blank" aria-label="Facebook"><i class="uil uil-facebook-f"></i></a>
+                    <a href="${person.instagram}" target="_blank" aria-label="Instagram"><i class="uil uil-instagram"></i></a>
+                    <a href="${person.linkedin}" target="_blank" aria-label="LinkedIn"><i class="uil uil-linkedin-alt"></i></a>
+                    <a href="${person.github}" target="_blank" aria-label="GitHub"><i class="uil uil-github-alt"></i></a>
+                </div>
+            </article>`;
+}
+
+// Global variables for search elements
+const searchInput = document.getElementById('search-input');
+let memberCards = []; 
+
+// Function to handle filtering
+function filterMembers() {
+    if (memberCards.length === 0) {
+        memberCards = document.querySelectorAll('.team__member');
+    }
+    
+    const query = searchInput.value.toLowerCase();
+    
+    memberCards.forEach(card => {
+        const cardText = card.textContent.toLowerCase(); 
+        
+        if (cardText.includes(query)) {
+            card.style.display = "";
+        } else {
+            card.style.display = "none";
+        }
+    });
+}
+
+// --- Event Listeners and Initial Setup ---
+document.addEventListener("DOMContentLoaded", function(){
+    const card_container_A = document.getElementById("card-container-A");
+    const card_container_B = document.getElementById("card-container-B");
+    let cards_A = "";
+    let cards_B = "";
+    
+    friends.forEach(person => {
+        if(person.sec === "A" || person.sec === "a") {
+            cards_A += createCard(person)
+        } 
+
+        if(person.sec === "B"  || person.sec === "b") {
+            cards_B += createCard(person)
+        } 
+
+    });
+    
+    // Set innerHTML only once after the loop for performance
+    if(card_container_A) {
+        card_container_A.innerHTML = cards_A;
+        card_container_B.innerHTML = cards_B;
+        
+        // Populate the memberCards array AFTER they are in the DOM
+        memberCards = document.querySelectorAll('.team__member');
+    }
+
+    if (searchInput) {
+        searchInput.addEventListener('input', filterMembers);
+    }
+});
+
 // start manu button toggle code
 const menuBtn = document.getElementById('menu-btn');
 const menuNav = document.getElementById('menu-nav');
@@ -17,63 +233,107 @@ const toggleMenu = () => {
     }
 };
 
-menuBtn.addEventListener('click', toggleMenu);
+if (menuBtn) {
+    menuBtn.addEventListener('click', toggleMenu);
+}
 
-const navLinks = menuNav.querySelectorAll('a');
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        if (window.innerWidth < 1024) { 
-            toggleMenu();
-        }
+if (menuNav) {
+    const navLinks = menuNav.querySelectorAll('a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (window.innerWidth < 1024) { 
+                toggleMenu();
+            }
+        });
     });
-});
+}
 // End of manu button toggle code
 
-// start search button code
-const searchInput = document.getElementById('search-input');
-const searchContainer = document.getElementById('search-container');
-const memberCards = document.querySelectorAll('.team__member');
-
-function filterMembers() {
-    const query = searchInput.value.toLowerCase();
-    memberCards.forEach(card => {
-        const name = card.querySelector('h4').textContent.toLowerCase();
-        const roll = card.querySelector('p').textContent.toLowerCase();
-        if (name.includes(query) || roll.includes(query)) {
-            card.style.display = "";
-        } else {
-            card.style.display = "none";
-        }
-    });
-}
-
-if (searchInput) {
-    searchInput.addEventListener('input', filterMembers);
-}
-
+// start search icon toggle code 
 document.addEventListener('DOMContentLoaded', function() {
     const searchIcon = document.getElementById('search-icon');
     const searchContainer = document.querySelector('.search-container');
-    const searchInput = document.getElementById('search-input');
-
-    searchIcon.addEventListener('click', function() {
-        // Toggle the 'expanded' class on the container
-        searchContainer.classList.toggle('expanded');
-
-        // Focus the input field when it expands, for immediate typing
-        if (searchContainer.classList.contains('expanded')) {
-            searchInput.focus();
-        } else {
-            // Blur the input when it collapses
-            searchInput.blur();
-        }
-    });
-
-    // Collapse the search bar when the user clicks outside of it
-    document.addEventListener('click', function(event) {
-        if (!searchContainer.contains(event.target) && searchContainer.classList.contains('expanded')) {
-            searchContainer.classList.remove('expanded');
-        }
-    });
+    
+    if (searchIcon && searchContainer && searchInput) {
+        searchIcon.addEventListener('click', function() {
+            searchContainer.classList.toggle('expanded');
+    
+            if (searchContainer.classList.contains('expanded')) {
+                searchInput.focus();
+            } else {
+                searchInput.blur();
+            }
+        });
+    
+        document.addEventListener('click', function(event) {
+            if (!searchContainer.contains(event.target) && searchContainer.classList.contains('expanded')) {
+                searchContainer.classList.remove('expanded');
+            }
+        });
+    }
 });
-// end search button code
+// end search icon toggle code
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const btw_container = document.getElementById('btwcardContainer');
+    const btw_close_btn = document.getElementById('btw-close-button');
+    const birthday_boy_girl_name = document.getElementById('btw-person-name');
+    const birthday_wish = document.getElementById('birthday-wish');
+
+    // Close button logic
+    if (btw_close_btn) {
+        btw_close_btn.addEventListener('click', function() {
+            if (btw_container) {
+                btw_container.classList.add('btw-card-closed');
+                setTimeout(() => {
+                    btw_container.style.display = "none";
+                    btw_container.classList.remove('btw-card-closed');
+                }, 500); 
+            }
+        });
+    }
+
+    // Date calculation (DD/MM format)
+    const today = new Date();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    const today_dob_format = `${day}/${month}`; 
+
+    let birthday_boy_girl = [];
+
+    // check is anyone's birthday match with current date
+    if (typeof friends !== 'undefined' && Array.isArray(friends)) {
+        for (const friend of classmates) {
+            if (friend.dob === today_dob_format) {
+                birthday_boy_girl.push(friend);
+            }
+        }
+    }
+    
+    // Check for birthdays and update UI
+    if (birthday_boy_girl.length !== 0 && btw_container && birthday_boy_girl_name) {
+        // Extract only the names from the friend objects
+        const namesArray = birthday_boy_girl.map(friend => friend.name);
+
+        // Format the names for display 
+        let namesText;
+        if (namesArray.length === 1) {
+            namesText = namesArray[0];
+        } else if (namesArray.length === 2) {
+            namesText = namesArray.join(' and ');
+        } else {
+            const last = namesArray.pop();
+            namesText = namesArray.join(', ') + `, and ${last}`;
+        }
+        
+        // Set the text and make the container visible (once)
+        birthday_boy_girl_name.innerText = `${namesText}`;
+        birthday_wish.innerText = `${randomWish}`;
+        btw_container.style.display = "flex";
+
+    } else if (btw_container) {
+        // No birthdays today
+        btw_container.style.display = "none";
+    }
+});
